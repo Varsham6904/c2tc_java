c5a847c639bce799a05d5790c8ef27958e5f758c
Varsham6904 <varshamrao69@gmail.com>
2025-09-21 20:27:57 +0530

    Create HierarchialDemo.java

---
+++ Inheritance/HierarchialDemo.java
@@ -0,0 +1,15 @@
+package com.tnsif.inheritance.hirarchial;
+
+import java.util.Scanner;
+
+public class HierarchialInheritanceDemo {
+	static int a;
+	
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		Employee e1=new Employee();
+		
+		System.out.println(a);
+	}
+
+}
